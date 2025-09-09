e76d762c9c6ea1b686fbcec133183d4206132ba5
Rayees Riyaz <rayeesriyaz343@gmail.com>
2025-09-09 15:26:46 +0530

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,52 @@
+class ArticlesController < ApplicationController
+def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  
+def create
+  @article = Article.new(params.require(:article).permit(:title, :description))
+  if @article.save
+    flash[:success] = "Article successfully created"
+    redirect_to @article
+  else
+    puts @article.errors.full_messages  
+    
+    render 'new', status: :unprocessable_entity 
+  end
+end
+
+
+ def update
+  @article = Article.find(params[:id])
+  if @article.update(params.require(:article).permit(:title, :description))
+    flash[:success] = "Article was successfully updated"
+    redirect_to @article
+  else
+    puts @article.errors.full_messages
+   
+    render 'edit',status: :unprocessable_entity
+  end
+end
+
+
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+   redirect_to article_path
+  flash[:success] = "Article deleted successfully"
+ 
+end
+end
