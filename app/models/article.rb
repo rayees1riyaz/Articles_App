da9a69b4f49307ed3efa88a94d69e2ad56b0c110
Rayees Riyaz <rayeesriyaz343@gmail.com>
2025-09-09 16:07:58 +0530

    add validations in users and articles table

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,11 @@
 class Article < ApplicationRecord
    validates :email, presence: true,       # must be filled
-                    uniqueness: true,     # no duplicate emails
+                    uniqueness: true, { case_sensitive: false },
                     format: { with: URI::MailTo::EMAIL_REGEXP, 
                               message: "must be a valid email address" }
 
   # Title validation
-  validates :name, presence: true,       # must be filled
-                    length: { minimum: 3, maximum: 50 } # between 3–50 chars
+  validates :name, presence: true,       
+                    length: { minimum: 3, maximum: 50 } 
 end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  # Name validation
+  validates :name, presence: true,
+                   uniqueness: true,
+                   length: { minimum: 3, maximum: 20 }
+
+  # Email validation
+  validates :email, presence: true,
+                    uniqueness: true, { case_sensitive: false },
+                    format: { with: URI::MailTo::EMAIL_REGEXP,
+                              message: "must be a valid email address" }
 end
