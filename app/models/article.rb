a9e8c33dd9b067553217042d6822686528be1444
Rayees Riyaz <rayeesriyaz343@gmail.com>
2025-09-09 15:47:03 +0530

    Add user Table

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,11 @@
 class Article < ApplicationRecord
+   validates :email, presence: true,       # must be filled
+                    uniqueness: true,     # no duplicate emails
+                    format: { with: URI::MailTo::EMAIL_REGEXP, 
+                              message: "must be a valid email address" }
+
+  # Title validation
+  validates :name, presence: true,       # must be filled
+                    length: { minimum: 3, maximum: 50 } # between 3–50 chars
+end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_09_094701) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_09_100857) do
   create_table "articles", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
