6d8b7cc458f98eb53c691501d1b01275a9becfd3
Rayees Riyaz <rayeesriyaz343@gmail.com>
2025-09-10 11:21:57 +0530

    Add password colum in users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,5 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false },
                     format: { with: URI::MailTo::EMAIL_REGEXP,
                               message: "must be a valid email address" }
+  has_secure_password
 end
